ad4c6e12231d85beb7aaaed23577ef5663114e61
1212wuhu <1392788316@qq.com>
2023-03-07 23:07:43 +0800

    第一版

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <scene/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Scene from './components/Scene_Component.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Scene
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+margin: 0%;
 }
 </style>
