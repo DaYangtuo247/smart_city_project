99ee7b28bd3296a2f402ba19bdc72e9de05d1bf3
吴奇灵 <2473605320@qq.com>
2023-06-29 12:31:34 +0800

    合并了一些vue组件

--- smart-city/src/components/left_graph.vue
+++ smart-city/src/components/left_graph.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="left-graph" v-show="show_library">
-        <div class="library" v-show="show_library">
+    <div class="left-graph" v-show="show_library || show_gongdi || show_wuhan">
+        <div v-show="show_library">
             <!-- <library_model></library_model> -->
             <library_analyse></library_analyse>
             <library_trend></library_trend>
             <library_rank></library_rank>
+        </div>
+        <div v-show="show_gongdi">
+            <gongdi_rank></gongdi_rank>
+            <gongdi_analyse></gongdi_analyse>
+            <gongdi_qushi></gongdi_qushi>
+        </div>
+        <div v-show="show_wuhan">
+            <wuhan_move></wuhan_move>
+            <wuhan_forest></wuhan_forest>
+            <wuhan_zhu></wuhan_zhu>
         </div>
     </div>
 </template>
@@ -13,9 +23,18 @@
 // import library_trend from "graph/library_trend.vue";
 import library_analyse from "graph/library_analyse.vue";
 import library_trend from "graph/library_trend.vue";
-import library_rank from "../graph/library_rank.vue";
+import library_rank from "graph/library_rank.vue";
 import library_type from "graph/library_type.vue";
 import library_model from "graph/library_model.vue";
+
+import gongdi_rank from "graph/gongdi_rank.vue";
+import gongdi_analyse from "graph/gongdi_analyse.vue";
+import gongdi_qushi from "graph/gongdi_qushi.vue";
+
+import wuhan_move from 'graph/wuhan_move.vue';
+import wuhan_forest from 'graph/wuhan_forest.vue';
+import wuhan_pointandline from 'graph/wuhan_pointandline.vue';
+import wuhan_zhu from 'graph/wuhan_zhu.vue';
 export default {
     components: {
         // library_model,
@@ -24,10 +43,22 @@ export default {
         library_analyse,
         library_trend,
         library_rank,
+
+        gongdi_rank,
+        gongdi_analyse,
+        gongdi_qushi,
+
+
+        wuhan_move,
+        wuhan_forest,
+        wuhan_pointandline,
+        wuhan_zhu
     },
     data() {
         return {
             show_library: false,
+            show_gongdi: false,
+            show_wuhan:false
         };
     },
     mounted() {
@@ -37,11 +68,14 @@ export default {
             console.log("this.show_library");
             eventBus.emit("show-library-data-l", this.show_library);
         });
-        // this.$eventBus.on("show-gongdi-graph-lr", url => {
-        //     this.show_library = !this.show_library;
-        //     console.log("this.show_gongdi");
-        //     // eventBus.emit("show-library-data-l", this.show_library);
-        // });
+        this.$eventBus.on("show-gongdi-graph-lr", url => {
+            this.show_gongdi = !this.show_gongdi;
+            eventBus.emit("show-gongdi-data-l", this.show_gongdi);
+        });
+        this.$eventBus.on("show-wuhan-graph-lr", url => {
+            this.show_wuhan = !this.show_wuhan;
+            eventBus.emit("show-wuhan-data-l", this.show_wuhan);
+        });
     },
 };
 </script>
